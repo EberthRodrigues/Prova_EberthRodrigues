5db9e5a9d0d385ea3fce5a5dbd8cbe344903bed8
Eberth da Silva Rodrigues <eberthsilva011@gmail.com>
2025-08-26 19:14:16 -0300

    inserindo cadastro fornecedor

---
+++ cadastro_fornecedor.php
@@ -0,0 +1,85 @@
+<?php
+    session_start();
+    require_once 'conexao.php';
+    
+    // verifica se o usuario tem permição
+    // Supondo que o peril seja ADM
+
+    if ($_SESSION['perfil']!= 1){
+        echo "Acesso negado";  
+            exit();
+    }
+
+    if ($_SERVER['REQUEST_METHOD']== 'POST'){
+        $nome_fornecedor = $_POST['nome_fornecedor'];
+        $email = $_POST['email'];
+        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+        $id_perfil = $_POST['id_perfil'];
+
+        $sql = "INSERT INTO usuario (nome, email, senha, id_perfil) VALUES (:nome,:email, :senha, :id_perfil)";
+
+        $stmtPerfil = $pdo -> prepare($sql);
+        
+        $stmtPerfil->bindParam(":nome", $nome);
+        $stmtPerfil->bindParam(":email", $email);
+        $stmtPerfil->bindParam(":senha", $senha);
+        $stmtPerfil->bindParam(":id_perfil", $id_perfil);
+
+        if ($stmtPerfil->execute()) {
+            echo "<script> alert('Usuario cadastro com sucesso!'); </script>";
+
+        } else{
+            echo "<script>alert('Erro ao cadastrar!'); </script>";
+
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css?v=2">
+
+</head>
+<body>
+
+<h2>Cadastrar usuário</h2>
+<form action="cadastro_usuario.php" method="POST">
+    <label for="nome">Nome: </label>
+    <input type="text" name="nome" id="nome" pattern="[A-Za-zÀ-ÿ ]+" title="Apenas letras e espaços são permitidos." required>
+
+    <label for="email">Email: </label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="senha">Senha: </label>
+    <input type="password" name="senha" id="senha" required>
+
+        <label for="id_perfil">Perfil: </label>
+            <select name="id_perfil" id="id_perfil" required>
+                <option value="1"> Administrador</option>
+                <option value="2"> Secretaria </option>
+                <option value="3"> Almoxarife </option>
+                <option value="4"> Cliente</option>
+            </select>
+            <button type="submit">Cadastrar</button><br>
+            <button type="reset">Cancelar</button>
+    
+    </form>
+    
+<script>
+const nomeInput = document.getElementById('nome');
+
+nomeInput.addEventListener('input', function() {
+    this.value = this.value.replace(/[^A-Za-zÀ-ÿ ]/g, '');
+});
+</script>
+
+
+
+<a href="principal.php">Voltar</a>
+<script src="validacoes.js"></script>
+
+</body>
+</html>
